8c4d10ece3d6f2993a41708c24e649cd6c12de09
Alex Tkach <tkach.sasha@mail.ru>
2017-05-16 20:32:05 +0300

    Update zadacha1.php

--- zadacha1.php
+++ zadacha1.php
@@ -1,3 +1,23 @@
+<form method="post">
+    <input type="text" name="datt">
+    <input type="submit" value="Отправить">   
+</form>
 <?php
-date_default_timezone_set('UTC');
-echo date('l jS \of F Y h:i:s A');
+if (!empty($_REQUEST['datt'])){
+    $datte = $_REQUEST['datt'];
+    $a = explode("-", $datte);
+    $d = $a[0];
+    $m = $a[1];
+    $y = $a[2];
+    if (checkdate($m,$d,$y)){
+        echo "Датта коректна " , $datte;
+    } else echo "Введите правильно датту.";
+    
+    
+}else
+    echo 'Введите датту';
+
+
+
+
+
